7c41002b758ab6cdf9e0a40b1856132bebeb08a2
mguptatempnice <mgupta6114@gmail.com>
2025-05-26 12:26:48 +0530

    update 3

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,16 @@
 import { MongoClient } from 'mongodb';
 
+// Extend the NodeJS.Global type to include _mongoClientPromise
+declare global {
+  // eslint-disable-next-line no-var
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
+
 const uri = process.env.MONGODB_URI!;
 const dbName = process.env.MONGODB_DB!;
 const client = new MongoClient(uri);
 
+// Use the global object to store the client promise
 const clientPromise: Promise<MongoClient> = global._mongoClientPromise || client.connect();
 global._mongoClientPromise = clientPromise;
 
